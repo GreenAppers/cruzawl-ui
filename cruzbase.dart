cda7fc56c1a8aedc53153ad640590c21d3619110
GreenAppers <GreenAppers@gmail.com>
2019-08-01 00:27:18 -0700

    Keep chart up to date

--- cruzbase.dart
+++ cruzbase.dart
@@ -5,114 +5,146 @@ import 'package:flutter_web/material.dart'
     if (dart.library.io) 'package:flutter/material.dart';
 
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:collection/collection.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 import 'package:cruzawl/currency.dart';
 import 'package:cruzawl/network.dart';
+import 'package:cruzawl/util.dart';
 
 import 'localization.dart';
 import 'model.dart';
 import 'ui.dart';
 
+enum CruzbaseBucketDuration { minute, hour }
+
 class CruzbaseWidget extends StatefulWidget {
   final Currency currency;
-  final BlockHeader tip;
   final Widget loadingWidget;
-  final Duration totalDuration, bucketDuration;
-  CruzbaseWidget(this.currency, this.tip,
+  final Duration windowDuration;
+  final CruzbaseBucketDuration bucketDuration;
+  CruzbaseWidget(this.currency,
       {this.loadingWidget,
-      this.totalDuration = const Duration(hours: 1),
-      this.bucketDuration = const Duration(minutes: 1)});
+      this.windowDuration = const Duration(hours: 1),
+      this.bucketDuration = CruzbaseBucketDuration.minute});
 
   @override
   _CruzbaseWidgetState createState() =>
-      _CruzbaseWidgetState(totalDuration, bucketDuration);
+      _CruzbaseWidgetState(windowDuration, bucketDuration);
 }
 
 class _CruzbaseWidgetState extends State<CruzbaseWidget> {
-  Duration totalDuration, bucketDuration;
-  List<charts.Series<TimeSeriesBlocks, DateTime>> series;
-  int totalBlocks;
+  SortedListSet<TimeSeriesBlocks> data;
+  int dataStartHeight, dataEndHeight;
+  DateTime dataStart, dataEnd, windowStart, windowEnd;
+  Duration windowDuration, animate = Duration(seconds: 5);
+  CruzbaseBucketDuration bucketDuration;
   bool loading = false;
-  BlockHeader last;
-  _CruzbaseWidgetState(this.totalDuration, this.bucketDuration);
+  var refresh;
+
+  _CruzbaseWidgetState(this.windowDuration, this.bucketDuration);
+
+  void clear() {
+    data = null;
+    windowStart = windowEnd = null;
+  }
 
   void setIntervalHourly() {
     if (!loading)
       setState(() {
-        totalDuration = const Duration(hours: 1);
-        bucketDuration = const Duration(minutes: 1);
-        series = null;
+        windowDuration = const Duration(hours: 1);
+        bucketDuration = CruzbaseBucketDuration.minute;
+        clear();
       });
   }
 
   void setIntervalDaily() {
     if (!loading)
       setState(() {
-        totalDuration = const Duration(days: 1);
-        bucketDuration = const Duration(hours: 1);
-        series = null;
+        windowDuration = const Duration(days: 1);
+        bucketDuration = CruzbaseBucketDuration.hour;
+        clear();
       });
   }
 
-  void load(Color color) async {
+  void addBlockToData(BlockHeader header) {
+    DateTime time = blockTime(header);
+    if (time.isBefore(dataStart)) dataStart = time;
+    if (time.isAfter(dataEnd)) updateDataEndTime(time);
+    TimeSeriesBlocks point = TimeSeriesBlocks(truncateTime(time, bucketDuration));
+    TimeSeriesBlocks prevPoint = data.find(point);
+    if (prevPoint != null)
+      prevPoint.block.add(header);
+    else {
+      point.block.add(header);
+      data.add(point);
+    }
+  }
+
+  void updateDataEndTime(DateTime newDataEnd) {
+    if (newDataEnd.isBefore(dataEnd)) return;
+    if (windowEnd.compareTo(dataEnd) >= 0) {
+      windowEnd = newDataEnd;
+      windowStart = windowEnd.subtract(windowDuration);
+    }
+    dataEnd = newDataEnd;
+  }
+
+  void load() async {
     if (loading || !widget.currency.network.hasPeer) return;
-    loading = true;
-    totalBlocks = 0;
-
-    final DateTime now = DateTime.now(), end = now.subtract(totalDuration);
-    final List<TimeSeriesBlocks> data = List<TimeSeriesBlocks>();
-    {
-      final int len = divideDuration(totalDuration, bucketDuration) + 1;
-      DateTime next = now;
-      for (int i = 0; i < len; i++) {
-        next = next.subtract(bucketDuration);
-        data.add(TimeSeriesBlocks(next));
-      }
-    }
-
-    last = null;
     Peer peer = await widget.currency.network.getPeer();
-    int fetchBlock = 50, height = widget.tip.height;
-    List<Future<BlockHeaderMessage>> blocks =
-        List<Future<BlockHeaderMessage>>(fetchBlock);
+    loading = true;
+    DateTime queryBackTo;
 
-    for (bool done = false; !done && height >= 0; /**/) {
-      int count = 0;
-      for (/**/; count < fetchBlock && height >= 0; count++)
-        blocks[count] = peer.getBlockHeader(height: height--);
-
-      for (int i = 0; i < count; i++) {
-        BlockHeaderMessage message = await blocks[i];
-        if (message == null) {
-          loading = false;
-          return;
-        }
-        BlockHeader header = message.header;
-        if ((done = !blockTime(header).isAfter(end))) break;
-
-        Duration offset = now.difference(blockTime(header));
-        int bucket = divideDuration(offset, bucketDuration);
-        if (bucket >= 0 && bucket < data.length) {
-          data[bucket].block.add(header);
-          totalBlocks++;
-          last = header;
-        }
+    if (data == null) {
+      dataStart = dataEnd = DateTime.now();
+      dataStartHeight = dataEndHeight = peer.tip.height;
+      data = SortedListSet<TimeSeriesBlocks>(
+          TimeSeriesBlocks.compareTime, List<TimeSeriesBlocks>());
+      queryBackTo = dataEnd.subtract(windowDuration);
+      windowStart ??= queryBackTo;
+      windowEnd ??= dataEnd;
+    } else {
+      updateDataEndTime(DateTime.now());
+      int tipHeight = peer.tip.height;
+      if (tipHeight > dataEndHeight) {
+        await fetch(peer, tipHeight, tipHeight - dataEndHeight);
+        dataEndHeight = tipHeight;
+        loading = false;
+        setState(() {});
+        return;
+      } else {
+        loading = false;
+        return;
       }
     }
 
-    series = [
-      charts.Series<TimeSeriesBlocks, DateTime>(
-        id: 'blocks',
-        colorFn: (_, __) => chartColor(color),
-        domainFn: (TimeSeriesBlocks blocks, _) => blocks.time,
-        measureFn: (TimeSeriesBlocks blocks, _) => blocks.blocks,
-        data: data,
-      )
-    ];
+    for (bool done = false; !done && dataStartHeight >= 0; /**/) {
+      List<BlockHeader> blocks = await fetch(peer, dataStartHeight, 50);
+      if (blocks == null || blocks.isEmpty) return;
+      done = !blockTime(blocks.last).isAfter(queryBackTo);
+      dataStartHeight -= blocks.length;
+    }
 
     loading = false;
     setState(() {});
+  }
+
+  Future<List<BlockHeader>> fetch(Peer peer, int height, int fetchBlock) async {
+    int count = 0;
+    List<Future<BlockHeaderMessage>> blocks =
+        List<Future<BlockHeaderMessage>>(fetchBlock);
+    for (/**/; count < fetchBlock && height >= 0; count++)
+      blocks[count] = peer.getBlockHeader(height: height--);
+
+    List<BlockHeader> ret = List<BlockHeader>(count);
+    for (int i = 0; i < count; i++) {
+      BlockHeaderMessage message = await blocks[i];
+      if (message == null) return null;
+      addBlockToData(message.header);
+      ret[i] = message.header;
+    }
+    return ret;
   }
 
   @override
@@ -121,38 +153,78 @@ class _CruzbaseWidgetState extends State<CruzbaseWidget> {
     final Cruzawl appState = ScopedModel.of<Cruzawl>(context);
     final ThemeData theme = Theme.of(context);
 
-    if (series == null) {
-      load(theme.accentColor);
+    bool loading = data == null || data.isEmpty;
+    load();
+
+    if (loading)
       return widget.loadingWidget ??
           SimpleScaffold(Center(child: CircularProgressIndicator()),
               title: locale.loading);
-    }
+
+    if (refresh == null)
+      refresh = Future.delayed(animate, () => setState(() => refresh = null));
+
+    TimeSeriesBlocks start = TimeSeriesBlocks(windowStart);
+    TimeSeriesBlocks end = TimeSeriesBlocks(windowEnd);
+    int endIndex =
+        lowerBound(data.data, start, compare: TimeSeriesBlocks.compareTime);
+    int startIndex =
+        lowerBound(data.data, end, compare: TimeSeriesBlocks.compareTime);
+    SortedListSet<TimeSeriesBlocks> window = SortedListSet<TimeSeriesBlocks>(
+        TimeSeriesBlocks.compareTime, data.data.sublist(startIndex, endIndex));
+    int totalBlocks = window.data.fold(0, (p, c) => p + c.blocks);
+    BlockHeader first = window.last.block.last;
+    BlockHeader last = window.first.block.first;
+    window.add(start, overwrite: false);
+    window.add(end, overwrite: false);
 
     return SimpleScaffold(
-        charts.TimeSeriesChart(
-          series,
-          animate: false,
-          defaultRenderer: charts.BarRendererConfig<DateTime>(),
-          defaultInteractions: false,
-          behaviors: [charts.SelectNearest(), charts.DomainHighlighter()],
-          selectionModels: [
-            charts.SelectionModelConfig(
-              type: charts.SelectionModelType.info,
-              changedListener: (charts.SelectionModel model) {
-                for (charts.SeriesDatum datum in model.selectedDatum)
-                  if (datum.datum.blocks > 0) {
-                    appState.navigateToHeight(
-                        context, datum.datum.block[0].height);
-                    break;
-                  }
-              },
-            ),
-          ],
+        GestureDetector(
+          child: charts.TimeSeriesChart(
+            [
+              charts.Series<TimeSeriesBlocks, DateTime>(
+                id: 'blocks',
+                colorFn: (_, __) => chartColor(theme.accentColor),
+                domainFn: (TimeSeriesBlocks blocks, _) => blocks.time,
+                measureFn: (TimeSeriesBlocks blocks, _) => blocks.blocks,
+                data: window.data,
+              )
+            ],
+            animate: true,
+            defaultRenderer: charts.BarRendererConfig<DateTime>(),
+            defaultInteractions: false,
+            behaviors: [
+              charts.SlidingViewport(),
+              charts.SelectNearest(),
+              charts.DomainHighlighter()
+            ],
+            selectionModels: [
+              charts.SelectionModelConfig(
+                type: charts.SelectionModelType.info,
+                changedListener: (charts.SelectionModel model) {
+                  for (charts.SeriesDatum datum in model.selectedDatum)
+                    if (datum.datum.blocks > 0) {
+                      appState.navigateToHeight(
+                          context, datum.datum.block.first.height);
+                      break;
+                    }
+                },
+              ),
+            ],
+            domainAxis: charts.DateTimeAxisSpec(
+                viewport:
+                    charts.DateTimeExtents(start: windowStart, end: windowEnd)),
+          ),
+          onHorizontalDragUpdate: (update) {
+            //debugPrint('horizontal drag update');
+          },
         ),
-        titleWidget: buildTitle(context));
+        titleWidget:
+            buildTitle(context, totalBlocks, first, last, last.height));
   }
 
-  Widget buildTitle(BuildContext context) {
+  Widget buildTitle(BuildContext context, int totalBlocks, BlockHeader first,
+      BlockHeader last, int tipHeight) {
     final Localization locale = Localization.of(context);
     final Cruzawl appState = ScopedModel.of<Cruzawl>(context);
     final ThemeData theme = Theme.of(context);
@@ -162,10 +234,10 @@ class _CruzbaseWidgetState extends State<CruzbaseWidget> {
         fontSize: 20,
         color: theme.primaryTextTheme.title.color,
         decoration: TextDecoration.underline);
-    final String duration = locale.formatDuration(totalDuration);
+    final String duration = locale.formatDuration(windowDuration);
 
     List<Widget> hashRate = <Widget>[
-      Text(locale.formatHashRate(last == null ? 0 : widget.tip.hashRate(last)),
+      Text(locale.formatHashRate(last == null ? 0 : last.hashRate(first)),
           style: titleStyle),
     ];
 
@@ -194,12 +266,12 @@ class _CruzbaseWidgetState extends State<CruzbaseWidget> {
     );
 
     List<Widget> heightEquals = buildLocalizationMarkupWidgets(
-      locale.heightEquals(widget.tip.height),
+      locale.heightEquals(tipHeight),
       style: titleStyle,
       tags: <String, LocalizationMarkup>{
         'a': LocalizationMarkup(
           style: linkStyle,
-          onTap: () => appState.navigateToHeight(context, widget.tip.height),
+          onTap: () => appState.navigateToHeight(context, tipHeight),
         ),
       },
     );
@@ -217,10 +289,13 @@ class _CruzbaseWidgetState extends State<CruzbaseWidget> {
 
 class TimeSeriesBlocks {
   DateTime time;
-  List<BlockHeader> block = List<BlockHeader>();
+  SortedListSet<BlockHeader> block = SortedListSet<BlockHeader>(
+      BlockHeader.compareHeight, List<BlockHeader>());
   TimeSeriesBlocks(this.time);
 
   int get blocks => block.length;
+
+  static int compareTime(dynamic a, dynamic b) => b.time.compareTo(a.time);
 }
 
 charts.Color chartColor(Color color) =>
@@ -231,3 +306,11 @@ int divideDuration(Duration q, Duration d) =>
 
 DateTime blockTime(BlockHeader header) =>
     DateTime.fromMillisecondsSinceEpoch(header.time * 1000);
+
+DateTime truncateTime(DateTime time, CruzbaseBucketDuration duration) =>
+    DateTime(
+        time.year,
+        time.month,
+        time.day,
+        time.hour,
+        duration == CruzbaseBucketDuration.minute ? time.minute : 0);
